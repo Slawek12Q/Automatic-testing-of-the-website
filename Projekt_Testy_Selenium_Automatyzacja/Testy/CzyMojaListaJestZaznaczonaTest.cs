b4065ec6b5d0a750f83f71710a9538ad7784100c
Slawek12Q <slawek.guzik@interia.pl>
2019-03-13 21:06:16 +0100

    Create CzyMojaListaJestZaznaczonaTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/CzyMojaListaJestZaznaczonaTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class CzyMojaListaJestZaznaczonaTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            Boolean czyZaznaczone = menuPoZalogowaniu
+                .przejdzDoMojegoKonta()
+                .czyMojaLista();
+
+            Assert.IsTrue(czyZaznaczone);
+        }
+    }
+}

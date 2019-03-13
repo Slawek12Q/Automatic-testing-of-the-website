af4749262487ec408d0fe59483ac026f28f1fd36
Slawek12Q <slawek.guzik@interia.pl>
2019-03-13 23:27:22 +0100

    Create OdznaczenieMojejListTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/OdznaczenieMojejListTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class OdznaczenieMojejListTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            Boolean czyZaznaczone = menuPoZalogowaniu
+                .przejdzDoMojegoKonta()
+                .kiknijWMyList()
+                .czyMojaLista();
+
+            Assert.IsFalse(czyZaznaczone);
+        }
+    }
+}

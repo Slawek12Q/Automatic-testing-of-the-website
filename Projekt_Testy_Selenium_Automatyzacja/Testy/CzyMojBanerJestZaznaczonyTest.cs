d273f9cd4364d63f8d55ca67e149d18af7de0d48
Slawek12Q <slawek.guzik@interia.pl>
2019-03-13 21:06:27 +0100

    Create CzyMojBanerJestZaznaczonyTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/CzyMojBanerJestZaznaczonyTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class CzyMojBanerJestZaznaczony : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            Boolean czyZaznaczone = menuPoZalogowaniu
+                .przejdzDoMojegoKonta()
+                .czyMojBanner();
+
+            Assert.IsTrue(czyZaznaczone);
+        }
+    }
+}

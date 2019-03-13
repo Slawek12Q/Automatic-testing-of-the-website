66f64d1fd1e3e9756b0c54f6e314f7ab7923f5d3
Slawek12Q <slawek.guzik@interia.pl>
2019-03-13 21:05:59 +0100

    Create BlednaProbaLogowaniaTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/BlednaProbaLogowaniaTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class BlednaProbaLogowaniaTest : BazaTestowa
+    {
+        [Test]
+        public void test()
+        {
+            StronaPoczatkowa poczatkowa = new StronaPoczatkowa();
+
+            poczatkowa.kliknijIWejdzNaStrone()
+                .kliknijWLinkDoLogowania()
+                .wprowadzLogin("ala")
+                .wprowadzHaslo("ela")
+                .kliknijWPrzyciskLogowania();
+
+            StronaLogowania stronaLogowania = new StronaLogowania();
+
+            String wiadomosc = stronaLogowania.pobierzWiadomosc();
+
+            Console.WriteLine(wiadomosc);
+
+            Assert.AreEqual("Invalid username or password. Signon failed.", wiadomosc);
+        }
+
+    }
+}

e9790f59a34db48c40c9aebd5a3def22d799f938
Slawek12Q <slawek.guzik@interia.pl>
2019-03-13 23:27:02 +0100

    Create OdznaczenieBaneruTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/OdznaczenieBaneruTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class OdznaczenieBaneruTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            Boolean czyZaznaczone = menuPoZalogowaniu
+                .przejdzDoMojegoKonta()
+                .kliknijMyBanner()
+                .czyMojBanner();
+
+            Assert.IsFalse(czyZaznaczone);
+        }
+    }
+}

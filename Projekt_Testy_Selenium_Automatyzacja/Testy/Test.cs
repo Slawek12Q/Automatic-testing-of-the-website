70acce343af01b0adea571e5b4a573c720ca992f
Slawek12Q <slawek.guzik@interia.pl>
2019-03-14 21:42:26 +0100

    Create Test.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//klasa testowan ma moje potrzeby testuje przyrost kodu, nie jest to zwiazane z projektem 
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class Test : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+       public void test()
+        {
+            StronaStopki stopki = new StronaStopki();
+            Boolean wiadomosc = stopki.czyBanerJestWyswietlony();
+
+            Assert.IsTrue(wiadomosc);
+          
+        }
+    }
+}

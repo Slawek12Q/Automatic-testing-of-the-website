6c8607ae68d849d090cf701e5f7ae9fd3e7e4864
Slawek12Q <slawek.guzik@interia.pl>
2019-03-14 21:42:02 +0100

    Create SprawdzCzyJestemyPrawidlowoZalogowaniTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/SprawdzCzyJestemyPrawidlowoZalogowaniTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class SprawdzCzyJestemyPrawidlowoZalogowaniTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            String uzytkownik = menuPoZalogowaniu.przejdzDoMojegoKonta()
+                .pobierzUzytkownika();
+
+            Assert.AreEqual(uzytkownik, "j2ee");
+        }
+        
+
+        
+    }
+}

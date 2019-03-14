949e78d5e0d64f6246d9375e3b0548afba5e1282
Slawek12Q <slawek.guzik@interia.pl>
2019-03-14 21:42:18 +0100

    Create SprawdzPoczatkowyJezykTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/SprawdzPoczatkowyJezykTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class SprawdzPoczatkowyJezykTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            String jezyk = menuPoZalogowaniu.przejdzDoMojegoKonta()
+                .zwrocJezyk();
+
+            Assert.AreEqual(jezyk, "english");
+        }
+    }
+}

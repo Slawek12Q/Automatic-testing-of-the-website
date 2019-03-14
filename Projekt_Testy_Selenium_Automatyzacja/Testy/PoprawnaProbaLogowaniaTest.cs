5c035a2b8088d16b858cc4d53359b1f73fab7edf
Slawek12Q <slawek.guzik@interia.pl>
2019-03-14 21:41:14 +0100

    Create PoprawnaProbaLogowaniaTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/PoprawnaProbaLogowaniaTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class PoprawnaProbaLogowaniaTest : BazaTestowa
+    {
+        [Test]
+        public void test()
+        {
+            StronaPoczatkowa poczatkowa = new StronaPoczatkowa();
+            Boolean czyBannerJestWidoczny = poczatkowa.kliknijIWejdzNaStrone()
+                .kliknijWLinkDoLogowania()
+                .wprowadzLogin("j2ee")
+                .wprowadzHaslo("j2ee")
+                .kliknijWPrzyciskLogowania()
+                .czyBanerJestWyswietlony();
+
+            Assert.IsTrue(czyBannerJestWidoczny);
+        }
+    }
+}

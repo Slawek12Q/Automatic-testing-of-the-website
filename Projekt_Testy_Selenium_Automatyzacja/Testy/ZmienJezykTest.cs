426fedc1a22efee1b9ac3cbe10d6a2482939ac29
Slawek12Q <slawek.guzik@interia.pl>
2019-03-15 22:43:15 +0100

    Create ZmienJezykTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/ZmienJezykTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class SZmienJezykTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            String jezyk = menuPoZalogowaniu.przejdzDoMojegoKonta()
+                .zmienjezyk("japanese")
+                .zwrocJezyk();
+
+            Assert.AreEqual(jezyk, "japanese");
+        }
+    }
+}

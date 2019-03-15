b84408b7776ddb3953da2034c4c3d526e3f58069
Slawek12Q <slawek.guzik@interia.pl>
2019-03-15 22:43:05 +0100

    Create WejscieNaStroneTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/WejscieNaStroneTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projekt_Testy_Selemium_Automatyzacja
+{
+    [TestFixture]
+    class WejscieNaStroneTest
+    {
+        [Test]
+        public void WejdzNaStroneISprawdzUrl ()
+        {
+            var driver = new ChromeDriver();
+            var adressUrl = "http://przyklady.javastart.pl/jpetstore/";
+            driver.Navigate().GoToUrl(adressUrl);
+            driver.Manage().Window.Maximize();
+            //wchodzimy na stronę przekazaną w zmiennej addressURL, i potem sprawdzamy za pomacą naszego drivera czy faktycznie 
+            //jestemy tam gdzie chcielismy
+            Assert.AreEqual(adressUrl, driver.Url);
+
+            driver.Close();
+
+        }
+    }
+}

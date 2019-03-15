b433df3f48dcd230ea492f4e0b18d1c43c72dd77
Slawek12Q <slawek.guzik@interia.pl>
2019-03-15 22:42:48 +0100

    Create WejdzNaStroneISprawdzTytulTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/WejdzNaStroneISprawdzTytulTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class WejdzNaStroneISprawdzTytulTest
+    {
+
+        [Test]
+        public void WejdzNaStroneISprawdzUrl()
+        {
+            var driver = new ChromeDriver();
+            var adressUrl = "http://przyklady.javastart.pl/jpetstore/";
+            driver.Navigate().GoToUrl(adressUrl);
+            driver.Manage().Window.Maximize();
+            //wchodzimy na stronę przekazaną w zmiennej i pobieramy jej tytuł, następnie sprawdzamy czy się zgadza
+            var tytul = driver.Title;
+            Assert.AreEqual(tytul, "JPetStore Demo");
+
+            driver.Close();
+
+        }
+    }
+}

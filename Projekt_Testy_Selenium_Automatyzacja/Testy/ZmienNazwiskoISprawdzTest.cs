87a342617df182e1de8e4828f05efbb848b6e683
Slawek12Q <slawek.guzik@interia.pl>
2019-03-15 22:43:32 +0100

    Create ZmienNazwiskoISprawdzTest.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/Testy/ZmienNazwiskoISprawdzTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.Testy
+{
+    [TestFixture]
+    class ZZmienNazwiskoISprawdzTest : PrzejscieDoZalogowanegoKonta
+    {
+        [Test]
+        public void test()
+        {
+            GorneMenuPoZalogowaniu menuPoZalogowaniu = new GorneMenuPoZalogowaniu();
+            menuPoZalogowaniu.przejdzDoMojegoKonta()
+                .zmienNazwisko("Kowalski")
+                .zatwierdzDane();
+
+            MojeKonto mojeKonto = new MojeKonto();
+
+            String wiadomosc = mojeKonto.pobierzNazwisko();
+
+            Assert.AreEqual(wiadomosc, "Kowalski");
+        }
+    }
+}

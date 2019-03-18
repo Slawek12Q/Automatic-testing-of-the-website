594929299c5c47671c7f1552a1534c7830dcf940
Slawek12Q <slawek.guzik@interia.pl>
2019-03-18 23:21:59 +0100

    Create GorneMenu.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/PageObjectPattern/GorneMenu.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern
+{
+    class GorneMenu
+    {
+        [FindsBy(How = How.CssSelector, Using = "#MenuContent a[href*='signonForm']")]
+        public IWebElement gorneMenu { get; set; }
+
+        public GorneMenu ()
+        {
+            PageFactory.InitElements(DriveManager.getDriver(), this);
+        }
+
+        public StronaLogowania kliknijWLinkDoLogowania()
+        {
+            gorneMenu.Click();
+
+            return new StronaLogowania();
+        }
+    }
+}

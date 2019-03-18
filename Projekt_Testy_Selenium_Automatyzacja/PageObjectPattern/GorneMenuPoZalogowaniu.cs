fe52a5c6577be59c758561ee6465aea4abed487b
Slawek12Q <slawek.guzik@interia.pl>
2019-03-18 23:22:07 +0100

    Create GorneMenuPoZalogowaniu.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/PageObjectPattern/GorneMenuPoZalogowaniu.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern
+{
+    class GorneMenuPoZalogowaniu
+    {
+        [FindsBy(How = How.CssSelector, Using = "div#MenuContent > a:nth-of-type(3)")]
+        public IWebElement mojeKonto { get; set; }
+        
+
+        public GorneMenuPoZalogowaniu()
+        {
+            PageFactory.InitElements(DriveManager.getDriver(), this);
+        }
+
+        public MojeKonto przejdzDoMojegoKonta()
+        {
+            mojeKonto.Click();
+            return new MojeKonto();
+        }
+
+    }
+}

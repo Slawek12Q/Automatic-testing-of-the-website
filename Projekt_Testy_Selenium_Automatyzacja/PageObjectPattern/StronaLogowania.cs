d0d5c1dd699e5a7ce45f8c1dadf60612226ba6c5
Slawek12Q <slawek.guzik@interia.pl>
2019-03-21 23:52:08 +0100

    Create StronaLogowania.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/PageObjectPattern/StronaLogowania.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern
+{
+    class StronaLogowania
+    {
+       [FindsBy(How = How.Name, Using = "username")]
+       public IWebElement poleDoImienia { get; set; }
+
+       [FindsBy(How=How.Name, Using = "password")]
+       public IWebElement poleDoHasla { get; set; }
+
+       [FindsBy(How=How.Name, Using = "signon")]
+       public IWebElement przyciskLogowania { get; set; }
+
+       [FindsBy(How=How.CssSelector, Using = "#Content ul[class='messages'] li")]
+       public IWebElement wiadomoscPoZalogowaniu { get; set; }
+
+
+        public StronaLogowania()
+        {
+            PageFactory.InitElements(DriveManager.getDriver(), this);
+        }
+
+        public StronaLogowania wprowadzLogin (string login)
+        {
+            poleDoImienia.Clear();
+            poleDoImienia.SendKeys(login);
+
+            return this;
+        }
+
+        public StronaLogowania wprowadzHaslo (string haslo)
+        {
+            poleDoHasla.Clear();
+            poleDoHasla.SendKeys(haslo);
+
+            return this;
+        }
+
+        public StronaStopki kliknijWPrzyciskLogowania()
+        {
+            przyciskLogowania.Click();
+
+            return new StronaStopki();
+        }
+
+        public String pobierzWiadomosc()
+        {
+            return wiadomoscPoZalogowaniu.Text;
+        }
+
+        public String pobierzZawartoscPolaLogowania()
+        {
+            return poleDoImienia.Text;
+        }
+
+
+
+
+    }
+}

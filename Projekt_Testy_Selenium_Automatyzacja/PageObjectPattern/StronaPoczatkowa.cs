6b2d550e64947440b6b5795fc93f6ac2f8b1af87
Slawek12Q <slawek.guzik@interia.pl>
2019-04-01 23:50:47 +0200

    Create StronaPoczatkowa.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/PageObjectPattern/StronaPoczatkowa.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern
+{
+    class StronaPoczatkowa
+    {
+        [FindsBy(How = How.CssSelector, Using = "#Content a")]
+        public IWebElement stronaPoczatkowa { get; set; }
+
+        public StronaPoczatkowa()
+        {
+            PageFactory.InitElements(DriveManager.getDriver(), this);
+        }
+
+        public GorneMenu kliknijIWejdzNaStrone ()
+        {
+            stronaPoczatkowa.Click();
+
+            return new GorneMenu();
+        }
+
+
+    }
+}

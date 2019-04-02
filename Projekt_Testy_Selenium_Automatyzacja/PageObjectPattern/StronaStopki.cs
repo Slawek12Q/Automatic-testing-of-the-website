48f8fa5a70e263cf4e93889283dfacdbd8b8c3e1
Slawek12Q <slawek.guzik@interia.pl>
2019-04-02 20:50:41 +0200

    Create StronaStopki.cs

---
+++ Projekt_Testy_Selenium_Automatyzacja/PageObjectPattern/StronaStopki.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using Projekt_Testy_Selemium_Automatyzacja.BazaTestow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Testy_Selemium_Automatyzacja.PageObjectPattern
+{
+    class StronaStopki
+    {
+        [FindsBy(How = How.CssSelector, Using = "#Banner img[src*='dog']")]
+        public IWebElement stronaStopki { get; set; }
+
+        public StronaStopki ()
+        {
+            PageFactory.InitElements(DriveManager.getDriver(), this);
+        }
+
+        public Boolean czyBanerJestWyswietlony()
+        {
+            Boolean czyWyswietlony = stronaStopki.Displayed;
+            return czyWyswietlony;
+        }
+    }
+}
